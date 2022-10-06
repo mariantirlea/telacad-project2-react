724e50157d0e070aeda1595d370e875d600ced4b
Marian Tirlea <mariantirlea@hotmail.com>
2022-10-06 20:25:06 +0300

    Implement react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router-dom/cjs/react-router-dom";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" component={Home} exact></Route>
+      <Route path="/about" component={About} exact></Route>
+      <Route path="/login" component={Login} exact></Route>
+      <Route component={PageNotFound}></Route>
+    </Switch>
   );
 }
 
