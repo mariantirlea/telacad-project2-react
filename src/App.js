794605e7f41c452547f017448dad9c9d7ba07947
Marian Tirlea <mariantirlea@hotmail.com>
2022-10-06 20:43:42 +0300

    Create header and footer and add css style

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
+    <>
+    <Header/>
     <Switch>
       <Route path="/" component={Home} exact></Route>
       <Route path="/about" component={About} exact></Route>
       <Route path="/login" component={Login} exact></Route>
       <Route component={PageNotFound}></Route>
     </Switch>
+    <Footer/>
+    </>
   );
 }
 
